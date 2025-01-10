c374b595012a4c2af711acf73b8f975199c4a592
Johan <johanduboiscontact@gmail.com>
2025-01-10 05:22:58 +0100

    Fixed static export config and ensured main page exists

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'export', // This is important for static export
+    output: 'export',
   };
   
   module.exports = nextConfig;
